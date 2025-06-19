3c4aea8d109c847944f571ae60c62861f3e05292
ValentinaDelgadoRincon <valentinadr1403@gmail.com>
2025-06-19 18:08:56 -0500

    feat: :sparkles: Add folder multimedia and second page
    add folder multimedia and second page for products
    
    By @Valentina

---
+++ main.js
@@ -0,0 +1,35 @@
+  async function cargarProductos() {
+    try {
+      const respuesta = await fetch('https://fakestoreapi.com/products');
+      const productos = await respuesta.json();
+
+      const contenedor = document.getElementById('product-container');
+
+      productos.forEach(producto => {
+        const box = document.createElement('div');
+        box.classList.add('box');
+
+        box.innerHTML = `
+          <span class="discount">Venta</span>
+          <div class="image">
+            <img src="${producto.image}" alt="${producto.title}">
+            <div class="icons">
+              <a href="login.html" class="fas fa-shopping-cart cart-btn">Agregar</a>
+            </div>
+          </div>
+          <div class="content">
+            <h3>${producto.title}</h3>
+            <div class="price">$${producto.price}</div>
+          </div>
+        `;
+
+        contenedor.appendChild(box);
+      });
+    } catch (error) {
+      console.error('Error al cargar productos:', error);
+    }
+  }
+
+
+  document.addEventListener('DOMContentLoaded', cargarProductos);
+
